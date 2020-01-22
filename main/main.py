2c001367e40279db08b3f6ad3ac3f6de498443be
Roshan Perera <ulprperera@yahoo.com>
2020-01-23 06:36:03 +1300

    Initial check in

---
+++ main/main.py
@@ -0,0 +1,22 @@
+ from ota_update.main.ota_updater import OTAUpdater
+
+
+    def download_and_install_update_if_available():
+        o = OTAUpdater('https://github.com/ulprperera/ESP32py')
+        o.download_and_install_update_if_available('RNA', 'Welcome2018!')
+
+
+    def start():
+        # your custom code goes here. Something like this: ...
+        # from main.x import YourProject
+        # project = YourProject()
+        # ...		
+		print ("ESP32 PICO Core says Hello!!!")
+
+
+    def boot():
+        download_and_install_update_if_available()
+        start()
+
+
+    boot()
